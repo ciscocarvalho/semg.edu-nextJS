be00ffa9f9b9af9a4f63b170667a1ff83931c21d
Francisco <francisco.carvalho.q@gmail.com>
2024-09-07 12:16:04 -0300

    refactor: use variant prop instead of another component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,11 @@
-import {AcmeLogoLarge} from '@/app/ui/acme-logo';
+import AcmeLogo from '../ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
  
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center h-screen">
       <div className="flex items-center justify-center max-w-[400px] flex-col space-y-2.5 p-4">  
-        <AcmeLogoLarge />
+        <AcmeLogo variant="large" />
         <LoginForm />
       </div>
     </main>
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -3,25 +3,28 @@ import Image from 'next/image';
 import logoSemgEdu from '@/public/SEMGEDU-Logo-Small.png'
 import logoSemgEduLarge from '@/public/SEMGEDU-Logo-Large.png'
 
-
-export default function AcmeLogo() {
-  return (    
-      <Image
-        src={logoSemgEdu}
-        width={200}  
-        height={200}
-        className="p-4"
-        alt="SEMG.edu company logo desktop version"
-      />
-  );
+interface AcmeLogoProps {
+  variant?: "default" | "large";
 }
 
-export function AcmeLogoLarge() {
-  return (    
+export default function AcmeLogo({ variant = "default" }: AcmeLogoProps) {
+  let src, width, height;
+
+  if (variant == "default") {
+    src = logoSemgEdu;
+    width = 200;
+    height = 200;
+  } else {
+    src = logoSemgEduLarge;
+    width = 300;
+    height = 300;
+  }
+
+  return (
       <Image
-        src={logoSemgEduLarge}
-        width={300}  
-        height={300}
+        src={src}
+        width={width}
+        height={height}
         className="p-4"
         alt="SEMG.edu company logo desktop version"
       />
